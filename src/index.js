95f2fa135d37870db1da7c343b8f63dfe55cbc71
Enrique <enrique@latamready.com>
2022-01-11 11:16:35 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// Settings
+app.set('port', process.env.PORT || 3000)
+app.set('json spaces', 2);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Routes
+//app.use(require('./routes/index'));
+app.use('/api', require('./routes/distritos'));
+app.use('/api', require('./routes/clientes'));
+app.use('/api', require('./routes/trabajadores'));
+app.use('/api', require('./routes/especialidades'));
+app.use('/api', require('./routes/proyectos'));
+
+// Starting the Server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const router = Router();
+
+// DB Conection
+var mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'todooficios',
+    user: 'root',
+    password: 'root',
+});
+
+conexion.connect(function (err) {
+    if (err) {
+        console.error('Error de conexion: ' + err.stack);
+        return;
+    }
+    console.log('Conectado con el identificador ' + conexion.threadId);
+});
+
+router.get('/', (req, res) => {
+    conexion.query(`SELECT * FROM distrito`, function (error, results, fields) {
+        if (error)
+            throw error;
+
+        res.json(results);
+    });
+});
+
+module.exports = router;
